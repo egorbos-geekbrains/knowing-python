15ca63f35a85096336fc466e8d98f6802b4463cc
Sergei Egorov <null@egorbos.codes>
2023-04-16 14:43:36 +1000

    Добавлено решение задачи № 2.4

--- Lesson 2/main.py
+++ Lesson 2/main.py
@@ -2,6 +2,16 @@
 def get_factorial(number: int):
     return 1 if number == 1 else number * get_factorial(number - 1)
 
+# Задача № 4
+def shift(list, steps):
+    if steps < 0:
+        steps = abs(steps)
+        for i in range(steps):
+            list.append(list.pop(0))
+    else:
+        for i in range(steps):
+            list.insert(0, list.pop())
+
 print("Задача № 1")
 number = int(input("Введите число: "))
 factorials = [get_factorial(i) for i in range(1, number + 1)]
@@ -21,3 +31,9 @@ second_line = input("Введите вторую строку: ").lower()
 for char in first_line:
     count_dict[char] = second_line.count(char)
 print(count_dict)
+
+print("\nЗадача № 4")
+number = abs(int(input("Введите число: ")))
+numbers = [i for i in range(-number, number + 1)]
+shift(numbers, 2)
+print(numbers)
