04aad57e5ce513480a9cfa72de256ad35fa8f434
Sergei Egorov <null@egorbos.codes>
2023-04-16 14:06:12 +1000

    Добавлено решение задачи № 2.2

--- Lesson 2/main.py
+++ Lesson 2/main.py
@@ -6,3 +6,10 @@ print("Задача № 1")
 number = int(input("Введите число: "))
 factorials = [get_factorial(i) for i in range(1, number + 1)]
 print(factorials)
+
+print("\nЗадача № 2")
+print("| X | Y | Z | ¬(X ∧ Y) ∨ Z |")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(f"| {x} | {y} | {z} |       {int(not (x and y) or z)}      |")
