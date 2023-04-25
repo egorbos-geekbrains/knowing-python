98ad8b9d3758ecb6d7fe7b5501c6078183a2e4ff
Sergei Egorov <null@egorbos.codes>
2023-04-25 17:52:13 +1000

    Добавлено решение задачи № 4.2

--- Lesson 4/main.py
+++ Lesson 4/main.py
@@ -1,5 +1,13 @@
 from math import sqrt
 
+# Вспомогательные функции
+def read_file_lines(filename: str):
+    with open(filename, encoding="utf-8") as file:
+        return file.readlines()
+
+def split_list_values(text: str, values_splitter: str = ','):
+    return text.strip().rstrip('\n').split(values_splitter)
+
 # Задача № 1
 def get_prime_factors(number: int):
     result = []
@@ -21,4 +29,10 @@ def get_prime_factors(number: int):
 print("Задача № 1")
 number = int(input("Введите число: "))
 prime_factors_list = get_prime_factors(number)
-print(prime_factors_list)
+print(prime_factors_list, f"-> {len(prime_factors_list)} шт.")
+
+print("\nЗадача № 2")
+icecream = read_file_lines("icecream.txt")
+assortment = set(split_list_values(icecream[0]))
+in_stock = set(split_list_values(icecream[1]))
+print(f"Закончилось: {', '.join(assortment.difference(in_stock))}")
