7ea1d6efeb2c4442bfd72558ab75a154a8558cfb
Sergei Egorov <null@egorbos.codes>
2023-04-29 12:27:45 +1000

    Добавлено решение задачи № 5.2

--- Lesson 5/main.py
+++ Lesson 5/main.py
@@ -1,7 +1,26 @@
 from random import randint
 
+# Задача № 2
+def get_ascending_sequences(num_list: list):
+    result = [[num_list[0]]]
+ 
+    for i in range(1, len(num_list)):
+        if num_list[i - 1] < num_list[i]:
+            result[-1].append(num_list[i])
+ 
+        else:
+            result.append([num_list[i]])
+
+    return list(filter(lambda e: len(e) > 1, result))
+
 print("Задача № 1")
 numbers = [randint(1, 10) for _ in range(7)]
 print(f"Оригинальный список: {numbers}")
 greater_five_list = list(filter(lambda e: e > 5, numbers))
 print(f"Список со значениями больше пяти: {greater_five_list}")
+
+print("\nЗадача № 2")
+numbers = [randint(1, 10) for _ in range(9)]
+print(f"Оригинальный список: {numbers}")
+print("Возрастающие последовательности: ", end='')
+print(*get_ascending_sequences(numbers), sep=', ')
