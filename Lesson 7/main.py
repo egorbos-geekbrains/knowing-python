1b06d1fbb90d1044f426795740a6052ab9f50a1c
Sergei Egorov <null@egorbos.codes>
2023-05-24 12:04:50 +1000

    Добавлено решение задачи № 7.2

--- Lesson 7/main.py
+++ Lesson 7/main.py
@@ -8,8 +8,22 @@ R = TypeVar('R')
 def map_to(func: Callable[[T], R], iterable: Iterable[T]):
     return list([func(e) for e in iterable])
 
+# Задача № 2
+def repeat_func(count: int):
+    def test(func):
+        def decorator():
+            for _ in range(0, count):
+                func()
+        return decorator
+    return test
+
 print('Задача № 1')
 numbers_list = [1, 2, 3, 4, 5]
 print(f'Список: {numbers_list}')
 numbers_list = map_to(lambda e: e * 10, numbers_list)
 print(f'Результат (e: e * 10): {numbers_list}')
+
+print('\nЗадача № 2')
+@repeat_func(10)
+def test_func(): print('Magic!', end=' ')
+test_func()
